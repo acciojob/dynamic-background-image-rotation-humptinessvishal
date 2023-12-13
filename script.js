19c8a84dc49500374a5afd97a00b3236104f738f
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-12-13 20:10:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function setBackgroundImage() {
+	const body = document.body;
+    const landscapeImage = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg';
+    const portraitImage = 'https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg';
+
+    const isLandscape = window.innerWidth > window.innerHeight;
+	body.style.backgroundImage = `url(${isLandscape ? landscapeImage : portraitImage})`;
+};
+setBackgroundImage();
+
+window.addEventListener('orientationchange', setBackgroundImage);
+window.addEventListener('resize', setBackgroundImage);
